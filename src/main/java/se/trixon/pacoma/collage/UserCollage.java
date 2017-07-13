51e4687ae28df038b1c83a996cbc4df3ed9453bd
Patrik Karlsson <patrik@trixon.se>
2017-07-13 08:58:00 +0200

    Add python code

--- src/main/java/se/trixon/pacoma/collage/UserCollage.java
+++ src/main/java/se/trixon/pacoma/collage/UserCollage.java
@@ -60,3 +60,40 @@ public class UserCollage {
 
     //TODO Clone deep copy
 }
+/*
+class UserCollage(object):
+    """Represents a user-defined collage
+
+    A UserCollage contains a list of photos (referenced by filenames) and a
+    collage.Page object describing their layout in a final poster.
+
+    """
+    def __init__(self, photolist):
+        self.photolist = photolist
+
+    def make_page(self, opts):
+        # Define the output image height / width ratio
+        ratio = 1.0 * opts.out_h / opts.out_w
+
+        # Compute a good number of columns. It depends on the ratio, the number
+        # of images and the average ratio of these images. According to my
+        # calculations, the number of column should be inversely proportional
+        # to the square root of the output image ratio, and proportional to the
+        # square root of the average input images ratio.
+        avg_ratio = (sum(1.0 * photo.h / photo.w for photo in self.photolist) /
+                     len(self.photolist))
+        # Virtual number of images: since ~ 1 image over 3 is in a multi-cell
+        # (i.e. takes two columns), it takes the space of 4 images.
+        # So it's equivalent to 1/3 * 4 + 2/3 = 2 times the number of images.
+        virtual_no_imgs = 2 * len(self.photolist)
+        no_cols = int(round(math.sqrt(avg_ratio / ratio * virtual_no_imgs)))
+
+        self.page = collage.Page(1.0, ratio, no_cols)
+        random.shuffle(self.photolist)
+        for photo in self.photolist:
+            self.page.add_cell(photo)
+        self.page.adjust()
+
+    def duplicate(self):
+        return UserCollage(copy.copy(self.photolist))
+*/
